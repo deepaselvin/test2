2cb331538f02842f463e06971cdb26ac39067c4c
Training <pradeepaselvin97@gmail.com>
2019-08-14 00:38:19 -0400

    input was changed

---
+++ src/github/Name.java
@@ -0,0 +1,13 @@
+package github;
+
+public class Name {
+
+	public static void main(String[] args) {
+ String name=" ";
+ if(name=="hello" )
+	 System.out.println("valid");
+ else if(name==" ")
+	 System.out.println("invalid");
+	}
+
+}
